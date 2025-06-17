b26cffb599c17b24898881167fefef0d1c535f36
klu0926 <lukuoyu@gmail.com>
2025-06-16 20:26:15 -0400

    update url

--- frontend/js/ascii.js
+++ frontend/js/ascii.js
@@ -1,8 +1,8 @@
 const BACKEND_URL = window.location.hostname === 'localhost'
-  ? 'http://localhost:5000'
+  ? 'http://127.0.0.1:5000'
   : 'https://klu0926.pythonanywhere.com';
 
-  
+
 document.addEventListener('DOMContentLoaded', () => {
   const fileInput = document.getElementById('fileInput');
   const preview = document.getElementById('preview');
